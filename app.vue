381fbf96a56107ab05d15bddfcd893a21a327b97
Clervens Volcy <clervens.volcy@gmail.com>
2025-02-21 20:03:58 -0500

    Added new logo and favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup>
+useHead({
+  title: 'Shy Smoke | AI',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [{ name: 'description', content: 'My amazing site' }],
+  link: [{ rel: 'icon', type: 'image/svg', href: '/logo-reverse.svg' }],
+})
+</script>
 <template>
   <div>
     <NuxtLayout>
