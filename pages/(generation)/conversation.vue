b57d7e275549434809cc7b7f5b8c3235146ba055
Clervens Volcy <clervens.volcy@gmail.com>
2025-02-24 15:20:01 -0500

    create heading component

--- pages/(generation)/conversation.vue
+++ pages/(generation)/conversation.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+const { data: product } = await useFetch(`/api/products/conversation`)
 </script>
 <template>
     <div class="container">
-        
+        <LayoutHeading
+            :title="product!.label"
+            :description="product!.description"
+            :bg-color="product!.bgColor"
+            :icon="product!.icon"
+            :icon-color="product!.iconColor"
+        />
+        <div class="px-4 lg:px-8">
+
+        </div>
     </div>
 </template>
