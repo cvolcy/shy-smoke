be2b7d293f783af7a987194efbdc0dfd626f844d
Clervens Volcy <clervens.volcy@gmail.com>
2025-03-01 20:45:34 -0500

    Added Replicate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
     BACKENDBASE_ADMIN_USER: process.env.BACKENDBASE_ADMIN_USER,
     BACKENDBASE_ADMIN_PASS: process.env.BACKENDBASE_ADMIN_PASS,
     OPENAI_API_KEY: process.env.OAI_API_KEY,
+    REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
     public: {
       BACKENDBASE_URL: process.env.BACKENDBASE_URL,
       BUILD_NUMBER: process.env.BUILD_NUMBER || process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7),
