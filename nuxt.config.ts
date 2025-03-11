2c904fba82991085c600cd9740dacc0be131e189
Clervens Volcy <clervens.volcy@gmail.com>
2025-03-10 20:28:10 -0400

    Added muxt font and vue-writer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/i18n'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/i18n', '@nuxt/fonts'],
   shadcn: {
     /**
      * Prefix for all the imported component
